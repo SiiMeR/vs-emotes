3a87aa3acd0738c68bdb5eb9e4ac69824a335d0a
Siim Raudsepp <siimraudsepp@msn.com>
2026-04-24 17:34:52 +0300

    Fix 1.22 CO compatibility

--- Emotes/CombatOverhaulPatch.cs
+++ Emotes/CombatOverhaulPatch.cs
@@ -1,4 +1,5 @@
-using CombatOverhaul.Animations;
+using System;
+using System.Linq;
 using HarmonyLib;
 using Vintagestory.API.Client;
 using Vintagestory.API.Common;
@@ -8,21 +9,41 @@ namespace Emotes;
 
 public static class CombatOverhaulPatch
 {
-    private const string HarmonyId = "emotes.combatoverhaul";
+    private const string HarmonyId = "emotes.overhaullib";
     private static Harmony _harmony;
 
     public static void Apply(ICoreClientAPI api)
     {
+        var overhaulAsm = AppDomain.CurrentDomain.GetAssemblies()
+            .FirstOrDefault(a => a.GetName().Name.Equals("overhaullib", StringComparison.OrdinalIgnoreCase));
+        if (overhaulAsm == null)
+        {
+            return;
+        }
+
+        var fpType = overhaulAsm.GetType("CombatOverhaul.Animations.FirstPersonAnimationsBehavior");
+        var tpType = overhaulAsm.GetType("CombatOverhaul.Animations.ThirdPersonAnimationsBehavior");
+        if (fpType == null && tpType == null)
+        {
+            return;
+        }
+
         _harmony = new Harmony(HarmonyId);
         var prefix = new HarmonyMethod(typeof(CombatOverhaulPatch), nameof(SkipIfEmotePlaying));
+        var paramTypes = new[] { typeof(Entity), typeof(ElementPose), typeof(AnimatorBase) };
 
-        var fpOnFrame = typeof(FirstPersonAnimationsBehavior).GetMethod(
-            "OnFrame", [typeof(Entity), typeof(ElementPose), typeof(AnimatorBase)]);
-        var tpOnFrame = typeof(ThirdPersonAnimationsBehavior).GetMethod(
-            "OnFrame", [typeof(Entity), typeof(ElementPose), typeof(AnimatorBase)]);
+        var fpOnFrame = fpType?.GetMethod("OnFrame", paramTypes);
+        var tpOnFrame = tpType?.GetMethod("OnFrame", paramTypes);
 
-        if (fpOnFrame != null) _harmony.Patch(fpOnFrame, prefix: prefix);
-        if (tpOnFrame != null) _harmony.Patch(tpOnFrame, prefix: prefix);
+        if (fpOnFrame != null)
+        {
+            _harmony.Patch(fpOnFrame, prefix);
+        }
+
+        if (tpOnFrame != null)
+        {
+            _harmony.Patch(tpOnFrame, prefix);
+        }
     }
 
     public static void Remove()
@@ -30,5 +51,8 @@ public static class CombatOverhaulPatch
         _harmony?.UnpatchAll(HarmonyId);
     }
 
-    private static bool SkipIfEmotePlaying() => !EmotesModSystem.IsEmotePlaying;
+    private static bool SkipIfEmotePlaying()
+    {
+        return !EmotesModSystem.IsEmotePlaying;
+    }
 }
